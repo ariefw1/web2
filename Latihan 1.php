1e495710e9dcd81324535ad1f5f22270edb51193
ariefw1 <wichaksonoarief@gmail.com>
2020-03-15 19:45:35 +0700

    publish Latihan 2

--- Latihan 1.php
+++ Latihan 1.php
@@ -1,9 +1,9 @@
 <?php
 $A = 123; // variable global
-funcition Test() {
+function Test() {
     $A="Test"; //variable local
     echo"Nilai A dalam fungsi =$A \n";
 }
 Test()
-echo"Nilai A luar fungsi =$A \n;
-?>
+echo "Nilai A luar fungsi =$A \n;
+>?
